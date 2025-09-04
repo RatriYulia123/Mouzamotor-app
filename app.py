cae77d8f1bf470f4453a2097a2051a88c50f7994
RatriYulia123 <yuliaratri20@gmail.com>
2025-09-04 14:10:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -225,26 +225,24 @@ if st.session_state.search_triggered and not st.session_state.filter_applied:
     df_rekomendasi = df[df['Klaster'].isin(klaster_cocok_ids)] 
     df_rekomendasi = df_rekomendasi[df_rekomendasi['Harga'] <= st.session_state.harga_input]
 
-    # Filter tahun
+   # Filter tahun dengan fallback (persis -> semua tahun di atas)
     if st.session_state.tahun_input != 'Semua':
         tahun_input_int = int(st.session_state.tahun_input)
-    
-    # Cari tahun sesuai
+
+    # motor dengan tahun persis
     df_tahun_sesuai = df_rekomendasi[df_rekomendasi['Tahun Produksi'] == tahun_input_int]
-    
-    # Filter tahun produksi"
-    if st.session_state.tahun_input != 'Semua':
-        tahun_input_int = int(st.session_state.tahun_input)
-  
-    df_tahun_sesuai = df_rekomendasi[df_rekomendasi['Tahun Produksi'] == tahun_input_int]
+
     if not df_tahun_sesuai.empty:
         df_rekomendasi = df_tahun_sesuai
     else:
+        # motor dengan tahun di atas input
         df_tahun_diatas = df_rekomendasi[df_rekomendasi['Tahun Produksi'] > tahun_input_int]
+
         if not df_tahun_diatas.empty:
             df_rekomendasi = df_tahun_diatas
-
-
+        else:
+            # kalau sama sekali nggak ada, kosongkan hasil -> biar nanti muncul warning
+            df_rekomendasi = pd.DataFrame()
 
     # Filter jenis
     if st.session_state.jenis_input != 'Semua':
@@ -348,6 +346,7 @@ if st.session_state.search_triggered and st.session_state.filter_applied:
 
 
         
+
 
 
 
